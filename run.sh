3455cbefb855018b3e1752c0b2abea4cc37a883a
Erwan <erwanclou@gmail.com>
2024-01-31 18:48:14 +0100

    Add support for sending wake-on-LAN packets from multiple network interfaces

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-INTERFACE_ETH=$(route | grep '^default' | grep -o '[^ ]*$')
 FORMATTED_MAC=$(echo $MAC | sed 's/-/:/g')
-etherwake -i $INTERFACE_ETH $FORMATTED_MAC
+
+interfaces=$(ifconfig | grep -o '^[^ ]*:' | sed 's/.$//')
+for interface in $interfaces
+do
+    echo "Sending from $interface to $FORMATTED_MAC"
+    etherwake -i $interface $FORMATTED_MAC
+done
