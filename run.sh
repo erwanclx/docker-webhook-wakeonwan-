0ff18b12c2d090666103a58bb4e4b1ae7a2b5aff
Erwan <erwanclou@gmail.com>
2024-01-31 19:24:33 +0100

    Commented out echo statement in run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ FORMATTED_MAC=$(echo $MAC | sed 's/-/:/g')
 interfaces=$(ifconfig | grep -o '^[^ ]*:' | sed 's/.$//')
 for interface in $interfaces
 do
-    echo "Sending from $interface to $FORMATTED_MAC"
+    # echo "Sending from $interface to $FORMATTED_MAC"
     etherwake -i $interface $FORMATTED_MAC
 done
